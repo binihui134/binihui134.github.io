278d422045e607e9755fd027b9ed96120e8a8f23
binihui134 <97980198+binihui134@users.noreply.github.com>
2024-08-09 18:14:47 +0000

    obfuscated haha

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
+// script.js
+
+import firebaseConfig from './obfusKeys.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDSuZBu0jGhWUTxndwi4AGobd0FIhE0j00",
-  authDomain: "loginserver-af1a9.firebaseapp.com",
-  projectId: "loginserver-af1a9",
-  storageBucket: "loginserver-af1a9.appspot.com",
-  messagingSenderId: "978682389438",
-  appId: "1:978682389438:web:a88c775a17f21131ac0fef",
-  measurementId: "G-1QEY7CFLL8"
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -48,8 +41,4 @@ window.OpenWindowRegister = () => {
 
 window.OpenWindowLogin = () => {
   window.location.href = 'login.html'; // Redirect to login page
-}
-
-window.Logout = () => {
-  window.location.href = "login.html";
 }
